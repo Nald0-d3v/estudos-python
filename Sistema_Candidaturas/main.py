936d91bf6bad1e1f7d35b0967d3d72cf9e7ed3af
Naldo Nobre <nobrenaldinho2@gmail.com>
2026-07-10 18:14:14 -0300

    Finaliza CRUD de candidaturas com SQLite

--- Sistema_Candidaturas/main.py
+++ Sistema_Candidaturas/main.py
@@ -21,16 +21,19 @@ def criar_tabela():
     conexao.close()
 
 
-def adicionar_candidatura(
-    empresa,
-    cargo,
-    salario,
-    modalidade,
-    status,
-    data
-):
+def adicionar_candidatura():
+
+    empresa = input("Nome da empresa: ")
+    cargo = input("Nome do cargo: ")
+    salario = int(input("Salario: "))
+    modalidade = input("Modalidade: ")
+    status = input("Status atual: ")
+    data = input("Data: ")
+
+
     conexao = sqlite3.connect("candidaturas.db")
     cursor = conexao.cursor()
+
 
     cursor.execute(
         """
@@ -83,15 +86,111 @@ def listar_candidaturas():
     conexao.close()
 
 
+def atualizar_status():
+    conexao = sqlite3.connect("candidaturas.db")
+    cursor = conexao.cursor()
+
+    cursor.execute("SELECT * FROM candidaturas")
+    candidatos = cursor.fetchall()
+
+    if not candidatos:
+        print("Nenhuma candidatura cadastrada.")
+        conexao.close()
+        return
+
+    for candidato in candidatos:
+        print("Empresa:", candidato[1], "- ID:", candidato[0])
+
+
+    status_validos = [
+    "enviado",
+    "em análise",
+    "entrevista",
+    "recusado",
+    "contratado"
+        ]
+
+    print()
+    print("Status disponiveis: ")
+    print("enviado")
+    print("em análise")
+    print("entrevista")
+    print("recusado")
+    print("contratado")
+    print()
+
+    while True:
+
+        identificar = input("Digite o ID da empresa que deseja mudar: ")
+        novo_status = input("Digite o status desejado: ")
+
+        if novo_status not in status_validos:
+            print("Status invalido")
+            continue
+
+        cursor.execute(
+            "UPDATE candidaturas SET status = ? WHERE id = ?",
+            (novo_status, identificar)
+        )
+
+        conexao.commit()
+        conexao.close()
+
+        print("Status atualizado com sucesso.")
+        break
+
+
+def remover_candidatura():
+    conexao = sqlite3.connect("candidaturas.db")
+    cursor = conexao.cursor()
+
+    cursor.execute("SELECT * FROM candidaturas")
+    candidatos = cursor.fetchall()
+
+    if not candidatos:
+        print("Nenhuma candidatura cadastrada.")
+        conexao.close()
+        return
+
+    for candidato in candidatos:
+        print("Empresa:", candidato[1], "- ID:", candidato[0])
+
+    identificar = input("Digite o ID da empresa que deseja exluir: ")
+
+    cursor.execute(
+    "DELETE FROM candidaturas WHERE id = ?",
+    (identificar,))
+
+    conexao.commit()
+    conexao.close()
+
+    print("Empresa excluida com sucesso.")
+
+    #entao remover seria assim?
+
+
 criar_tabela()
 
-# adicionar_candidatura(
-#     "Google",
-#     "Analista",
-#     2500,
-#     "Remoto",
-#     "Enviado",
-#     "09/07/2026"
-# )
+while True:
+    menu = input("Comando: ").strip().lower()
 
-listar_candidaturas()
+
+    if menu == 'sair':
+        print("Encerrando programa.")
+        break
+
+    elif menu == 'atualizar status':
+
+        atualizar_status()
+
+    elif menu == 'adicionar candidatura':
+
+        adicionar_candidatura()
+
+    elif menu == 'remover candidatura':
+        
+        remover_candidatura()
+
+    elif menu == 'listar candidatura':
+
+        listar_candidaturas() 
